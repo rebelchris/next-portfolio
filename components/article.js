b0ae65f90a71613d884ae812774356bd972e4b1b
Chris Bongers <chrisbongers@gmail.com>
2022-10-10 07:23:38 +0200

    fix: markdown powered blog

--- components/article.js
+++ components/article.js
@@ -1,12 +1,12 @@
-export default function Article({className = 'rounded-lg'}) {
+import Link from "next/link";
+
+export default function Article({post, className = 'rounded-lg'}) {
     return (
         <article className={`bg-white p-4 ${className}`}>
-            <h3 className='text-2xl mb-2 font-medium'>Post Title</h3>
-            <span className='text-gray-600 mb-4 block'><date>12 Feb 21</date> | Design System</span>
+            <Link href={`blog/${post.slug}`}><h3 className='text-2xl mb-2 font-medium hover:text-red-400 cursor-pointer'>{post.title}</h3></Link>
+            <span className='text-gray-600 mb-4 block'><date>{post.date}</date> | {post.tags.map(tag => tag).join(', ')}</span>
             <p>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-                sint. Velit officia consequat duis enim velit mollit. Exercitation
-                veniam consequat sunt nostrud amet.
+                {post.description}
             </p>
         </article>
     );
