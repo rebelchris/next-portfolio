610c51226162a72ece21e816c5fd0040e8a510ee
Chris Bongers <chrisbongers@gmail.com>
2022-10-13 07:45:09 +0200

    fix: entity escape

--- components/introHeader.js
+++ components/introHeader.js
@@ -3,7 +3,7 @@ export default function IntroHeader() {
         <header className='px-6 py-14'>
             <div className='max-w-4xl mx-auto flex gap-14 items-center flex-col-reverse md:flex-row'>
                 <div>
-                    <h1 className='text-3xl font-bold mb-6'>Hi, I'm Chris!<br />Creative Developer</h1>
+                    <h1 className='text-3xl font-bold mb-6'>Hi, I&apos;m Chris!<br />Creative Developer</h1>
                     <p className='text-lg mb-6'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                     <a href='#' className='inline-block bg-red-400 text-white px-6 py-3 text-lg rounded'>Check my work</a>
                 </div>
