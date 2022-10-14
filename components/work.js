bae4d0f11b5e778a7f9f292cbe5f7aaac07b8aeb
Chris Bongers <chrisbongers@gmail.com>
2022-10-14 07:59:44 +0200

    fix: next-image

--- components/work.js
+++ components/work.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function Work({item}) {
     return (
-        <article className='flex items-center border-b-2 py-6'>
-            <img src={item.image} className='w-1/3 mr-6 rounded-lg' />
+        <article className='flex flex-col md:flex-row items-center border-b-2 py-6'>
+            <div className='w-full md:w-1/3 mb-3 md:mr-6'>
+                <Image src={item.image} className='rounded-lg' layout="responsive" width={500} height={335} />
+            </div>
             <div>
                 <h3 className='text-2xl mb-2 font-medium'>{item.title}</h3>
                 <span className='text-gray-600 mb-4 block'>
